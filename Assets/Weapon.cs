6d60b5f3ac302d2c8afa4e95f3e06401ba6de3f1
Louisa Brehm <louisa.brehm@student.tugraz.at>
2026-02-20 11:36:39 +0100

    Initial Unity project (clean)

---
+++ Assets/Weapon.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, 200*Time.deltaTime);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity_Game
+This will become a minimalistic game designed with Unity and C#
