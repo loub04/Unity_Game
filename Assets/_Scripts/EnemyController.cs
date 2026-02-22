df5fe3c51dbe34ec8b2be84ef50a40aff1d347eb
Louisa Brehm <louisa.brehm@student.tugraz.at>
2026-02-22 20:16:18 +0100

    spawning enemys

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab;
+    [SerializeField]
+    private int enemyCount = 5;
+    [SerializeField]
+    private Transform spawnTopLeft, spawnTopRight, spawnBottomLeft, spawnBottomRight;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for(int i = 0; i < enemyCount; i++)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Vector3 spawnPosition = SelectRandomPosition();
+        GameObject enemyObject = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+    }
+
+    private Vector3 SelectRandomPosition()
+    {
+        Transform selectedTransform = null;
+        int randomValue = Random.Range(0, 4);
+        switch(randomValue)
+        {
+            case 0:
+                selectedTransform = spawnTopLeft;
+                break;
+            case 1:
+                selectedTransform = spawnTopRight;
+                break;
+            case 2:
+                selectedTransform = spawnBottomLeft;
+                break;
+            case 3:
+                selectedTransform = spawnBottomRight;
+                break;
+            default:
+                selectedTransform = spawnBottomLeft;
+                break;
+        }
+        return selectedTransform.position + (Vector3)Random.insideUnitCircle;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
